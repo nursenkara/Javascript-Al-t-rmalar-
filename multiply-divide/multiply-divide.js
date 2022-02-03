26884c8994a19ea21721b4dcc2c2e22631a3b718
Nur Şenkara <nsenkara19@gmail.com>
2022-02-03 14:45:09 +0300

    Javascript alıştırmaları

---
+++ multiply-divide/multiply-divide.js
@@ -0,0 +1,12 @@
+function Multiply_Two_Num(){
+    var first = document.getElementById("firstNumber").value;
+    var second = document.getElementById("secondNumber").value;
+    var result = document.getElementById("result");
+    result.innerHTML = first*second;
+}
+function Divide_Two_Num(){
+    var first = document.getElementById("firstNumber").value;
+    var second = document.getElementById("secondNumber").value;
+    var result = document.getElementById("result");
+    result.innerHTML = first/second;
+}
